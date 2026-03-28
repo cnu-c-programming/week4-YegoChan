4e8fe933a96496be420fc84831c17f6f84028c81
Yego Chan <136900567+YegoChan@users.noreply.github.com>
2026-03-28 14:25:16 +0900

    Update 4-3.c

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -9,13 +9,16 @@ int main() {
         void* addr_1 = &(arr[i + 1]);
         /* 서로 인접한 두 요소의 실제 주소 */
         printf("address of arr[%d]: %p\n", i, addr_0);
-        printf("address of arr[%d + 1]: %p\n", i, addr_1);
+        printf("address of arr[%d+1]: %p\n", i, addr_1);
         /* 서로 인접한 두 요소의 주소 차이 */
-        printf("&(arr[%d]) - &(arr[%d + 1]) = %d\n", i, i + 1, (unsigned long long) addr_1 - (unsigned long long) addr_0);
+        printf("&(arr[%d]) - &(arr[%d+1]) = %d\n", i, i + 1, (unsigned long long) addr_1 - (unsigned long long) addr_0);
         printf("\n");
     }
 
     return 0;
 }
 
-// int 정수형이니까 4바이트씩 차이난다고?
+/*
+4바이트씩 차이남. 근데, 이미 %d / %d+1라고 적어놓고 i / i+1을 대입하니까 결과가 이상해짐..
+또 arr[%d] - arr[%d+1]이 아니고, arr[%d+1] - arr[%d]가 되어야 할 거 같음..
+*/
